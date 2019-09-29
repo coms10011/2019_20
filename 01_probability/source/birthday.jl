16c0fa9ceae60ee584707ba8c49c3dabdf3a8816
Conor Houghton <cscjh@localhost.localdomain>
2019-09-30 00:54:33 +0100

    copied from last year

---
+++ 01_probability/source/birthday.jl
@@ -0,0 +1,19 @@
+
+function probability(n::Int64)
+
+    p_none=1.0::Float64
+
+    for i in 1:n
+        p_none=p_none*((365+1-i)/365.0)
+    end
+    
+    1-p_none
+
+end
+
+n=1
+
+while n<60
+    println(n," ",probability(n))
+    n+=1
+end
